bf652fa4afaf2bbc1fd4ad32f021691c5355ac3e
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-06 22:22:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
 def reverse_each_word(string)
 
-
+new_sentence = []
 new_array = string.split 
 
-new_array.collect do |word|
-  word.reverse
+new_array.each do |word|
   
+  new_sentence << word.reverse
   
 end
 
-new_array.join(" ")
+new_sentence.join(" ")
 
 end
